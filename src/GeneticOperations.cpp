159d404c0d8592ef9731773bab84b3a5c3439eb6
Miguel Martin <miguel@miguel-martin.com>
2015-03-23 18:22:59 +1030

    Making progress to mating

--- src/GeneticOperations.cpp
+++ src/GeneticOperations.cpp
@@ -1,5 +1,7 @@
 #include "GeneticOperations.hpp"
 
+#include <queue>
+
 #include "lang/Node.hpp"
 
 template <class F>
@@ -32,49 +34,17 @@ void visit(NodePtr& node, F f)
     visit(operatorNode.right, f);
 }
 
-int depth(const NodePtr& node)
-{
-    // note: I could write this in terms on visit_const,
-    // however I feel like that's unnecessary overhead for the compiler to optimize
-    // also it makes the code a bit more un-readable
-    if(node->type() != Node::Type::OPERATION)
-    {
-        return 1;
-    }
+int depth(const NodePtr& node);
 
-    OperatorNode& operatorNode = *static_cast<OperatorNode*>(node.get());
-    int leftDepth = depth(operatorNode.left);
-    int rightDepth = depth(operatorNode.right);
-    return 1 + leftDepth > rightDepth ? leftDepth  : rightDepth ;
-}
+int nodeCount(const NodePtr& node);
 
-int node_count(const NodePtr& node)
-{
-    int count = 0;
-    visit(node, [&](const NodePtr&) { ++count; });
-    return count;
-}
+std::vector<int> randomNodes(RandomEngine& engine, const NodePtr& node);
 
 Function& mutate(RandomEngine& engine, Function& fn, std::uniform_int_distribution<> constantDist)
 {
     static std::uniform_int_distribution<> operatorDist(0, (int)Operator::COUNT - 1);
 
-    const int NODE_COUNT = node_count(fn.getNode());
-    const int amountOfNodesToChange = std::uniform_int_distribution<>(1, NODE_COUNT)(engine);
-
-    std::vector<int> nodesToChange(amountOfNodesToChange, -1);
-    std::uniform_int_distribution<> dist(0, NODE_COUNT);
-    for(auto& node : nodesToChange)
-    {
-        int newNode;
-        do
-        {
-            newNode = dist(engine);
-        }
-        while (std::find(nodesToChange.begin(), nodesToChange.end(), node) == std::end(nodesToChange));
-        
-        node = newNode;
-    }
+    auto nodesToChange = randomNodes(engine, fn.getNode());
 
     int currentNode = -1;
     visit(fn.getNode(), [&](NodePtr& node)
@@ -126,5 +96,121 @@ Function& mutate(RandomEngine& engine, Function& fn, std::uniform_int_distributi
 
 Function mate(RandomEngine& engine, const Function& p1, const Function& p2)
 {
-    return Function("TODO");
+    const Function* parents[] = { &p1, &p2 };
+    std::vector<int> nodesToChange[] = { randomNodes(engine, p1.getNode()), randomNodes(engine, p2.getNode()) };
+
+    std::queue<NodePtr> singleNodes;
+    std::vector<NodePtr> operatorNodes;
+
+    NodePtr rootNode;
+
+    for(int i = 0; i < 2; ++i)
+    {
+        auto& listOfNodes = nodesToChange[i];
+        auto& parent = *parents[i];
+        int currentNode = -1;
+
+        visit(parent.getNode(), [&](NodePtr& node)
+                {
+                    ++currentNode;
+                    if(std::find(listOfNodes.begin(), listOfNodes.end(), currentNode) == std::end(listOfNodes))
+                    {
+                        return; 
+                    }
+
+                    // ughhhhhhh TODO
+                    switch(node->type())
+                    {
+                        case Node::Type::OPERATION:
+                            if(singleNodes.size() >= 2)
+                            {
+                                 
+                            } 
+                            else
+                            {
+                                operatorNodes.push_back(node); 
+                            }
+                            break;
+                        case Node::Type::VARIABLE:
+                        case Node::Type::VALUE:
+                            singleNodes.push(node);
+
+                            for(auto& n : operatorNodes)
+                            {
+                                auto op = static_cast<OperatorNode*>(n.get());
+                                if(op->left == nullptr)
+                                {
+                                    auto& nodeToPutIn = singleNodes.front();
+                                    singleNodes.pop();
+                                    op->left = nodeToPutIn;
+                                    break;
+                                }
+
+                                if(op->right == nullptr)
+                                {
+                                    auto& nodeToPutIn = singleNodes.front();
+                                    singleNodes.pop();
+                                    op->right = nodeToPutIn;
+                                    break;
+                                }
+                            }
+
+                            break;
+                        default:
+                            assert(0 && "Oops. I dunno how this even occurred");
+                            break;
+                    }
+                }
+        );
+    }
+
+    // if we pick zero operator nodes then we shall create an operator node
+    // if we pick one operator node, then we shall choose half of the operator node (50:50)
+    // if we pick two operator nodes then we shall do the above but using one of the two operators (50:50)
+    
+    return Function(rootNode);
+}
+
+int depth(const NodePtr& node)
+{
+    // note: I could write this in terms on visit_const,
+    // however I feel like that's unnecessary overhead for the compiler to optimize
+    // also it makes the code a bit more un-readable
+    if(node->type() != Node::Type::OPERATION)
+    {
+        return 1;
+    }
+
+    OperatorNode& operatorNode = *static_cast<OperatorNode*>(node.get());
+    int leftDepth = depth(operatorNode.left);
+    int rightDepth = depth(operatorNode.right);
+    return 1 + leftDepth > rightDepth ? leftDepth  : rightDepth ;
+}
+
+int nodeCount(const NodePtr& node)
+{
+    int count = 0;
+    visit(node, [&](const NodePtr&) { ++count; });
+    return count;
+}
+
+std::vector<int> randomNodes(RandomEngine& engine, const NodePtr& node)
+{
+    const int NODE_COUNT = nodeCount(node);
+    const int amountOfNodesToChange = std::uniform_int_distribution<>(1, NODE_COUNT)(engine);
+
+    std::vector<int> nodes(amountOfNodesToChange, -1);
+    std::uniform_int_distribution<> dist(0, NODE_COUNT);
+    for(auto& node : nodes)
+    {
+        int newNode;
+        do
+        {
+            newNode = dist(engine);
+        }
+        while (std::find(nodes.begin(), nodes.end(), node) == std::end(nodes));
+        
+        node = newNode;
+    }
+    return nodes;
 }
