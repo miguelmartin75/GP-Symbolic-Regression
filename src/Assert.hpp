c5aebdd51c7cd73b4dbca16dad0d1f954cfbe29c
Miguel Martin <miguel@miguel-martin.com>
2015-04-07 16:01:11 +0930

    Removed src dir

--- src/Assert.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef ASSERT_HPP
-#define ASSERT_HPP
-
-#include <cassert>
-
-#define ASSERT(true_cond, msg) assert((true_cond) && msg)
-
-#endif // ASSERT_HPP
--- src/random_utils.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef RANDOM_UTILS_HPP
-#define RANDOM_UTILS_HPP
-
-#include <random>
-
-using RandomEngine = std::default_random_engine;
-
-#endif // RANDOM_UTILS_HPP
