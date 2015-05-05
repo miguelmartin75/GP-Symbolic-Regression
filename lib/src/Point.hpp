bbfe9d00f0080ad1aaa1c232f217067090d1c01a
Miguel Martin <miguel@miguel-martin.com>
2015-05-05 20:51:12 +0930

    removed unnecessary/possibly buggy comparisions

--- lib/src/Point.hpp
+++ lib/src/Point.hpp
@@ -15,21 +15,6 @@ inline bool operator<(const Point& p1, const Point& p2)
     return p1.x < p2.x;
 }
 
-inline bool operator>=(const Point& p1, const Point& p2)
-{
-    return !operator<(p1, p2);
-}
-
-inline bool operator<=(const Point& p1, const Point& p2)
-{
-    return !operator<(p2, p1);
-}
-
-inline bool operator>(const Point& p1, const Point& p2)
-{
-    return !operator<=(p1, p2);
-}
-
 inline bool operator==(const Point& p1, const Point& p2)
 {
     return p1.x == p2.x && p2.y == p2.y;
