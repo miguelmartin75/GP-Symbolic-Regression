3df4ca2aa96df875a58110bcc72724c56ed50ea9
Miguel Martin <miguel@miguel-martin.com>
2015-05-30 20:21:26 +0930

    Remove string_view.hpp

--- lib/src/string_view.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef STRING_VIEW_HPP
-#define STRING_VIEW_HPP
-
-#include "extlibs/string_view/include/experimental/string_view"
-
-using string_view = std::experimental::string_view;
-
-#endif // STRING_VIEW_HPP
